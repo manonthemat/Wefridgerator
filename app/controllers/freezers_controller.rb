a8db8be8c369d4e5cf970ce3d4e048dac8bf956a
manonthemat <empower@manonthemat.com>
2014-09-07 17:36:08 -0700

    m2x bugfix in freezersctrl

--- app/controllers/freezers_controller.rb
+++ app/controllers/freezers_controller.rb
@@ -4,8 +4,8 @@ class FreezersController < ApplicationController
   @@m2x = M2X.new('960e2f4da5d1cee68a17b65eb2127cba')
 
   def getTemp
-    fresh = @@m2x.feeds.streams(feed).json['streams'].first['value']
-    freezer = @@m2x.feeds.streams(feed).json['streams'].last['value']
+    fresh = @@m2x.feeds.streams(@@feed).json['streams'].first['value']
+    freezer = @@m2x.feeds.streams(@@feed).json['streams'].last['value']
     result = "#{fresh} #{freezer}"
     result = result.to_json
     render json: result
